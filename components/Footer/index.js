5d6ac7e887f529940bc76f808086fcb36e4ec165
Uriel <urieligara@hotmail.com>
2022-12-08 02:12:08 -0300

    copyright updated

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,15 +3,13 @@ import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
   const { t } = useTranslation();
-  const d = new Date();
-  let year = d.getFullYear();
   return (
     <footer className='bg-defaultBlack2'>
       <div className='default-container'>
         <div className='flex text-base py-4 flex-col lg:flex-row place-items-center'>
           <div className='flex flex-grow place-items-center'>
             <span className='px-2'>
-              © {year} <span className='font-bold'>{t('footer.copyright.name')}</span>.{' '}
+              Copyright © 2022 <span className='font-bold'>{t('footer.copyright.name')}</span>.{' '}
               {t('footer.copyright.all-rights-reserved')}
             </span>
           </div>
