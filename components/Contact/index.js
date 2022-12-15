4a5082446c23970b6fd005c8c8350f3e665ad264
Uriel <urieligara@hotmail.com>
2022-12-15 20:27:02 -0300

    classname updated

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Contact() {
   const { t } = useTranslation();
   return (
-    <section id='contact' className='py-10 text-base'>
+    <section id='contact' className='pb-10 text-base'>
       <div className='default-container'>
         <div className='text-center flex flex-col items-center py-6'>
           <h1 className='text-base'>{t('contact.title')}</h1>
