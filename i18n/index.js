c114b815b76b37666daab2cef4368536c48913ce
Uriel Guimarães <urieligara@hotmail.com>
2023-06-21 08:01:59 -0300

    ptBR locales removed

--- i18n/index.js
+++ i18n/index.js
@@ -1,10 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import ptBR from './locales/pt-br.json';
 import enUS from './locales/en-us.json';
 
 const resources = {
-  'pt-BR': ptBR,
   'en-US': enUS,
 };
 
