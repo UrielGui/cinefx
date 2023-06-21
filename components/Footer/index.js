41a790974a19411c00e188dffd95987eb612529d
Uriel Guimarães <urieligara@hotmail.com>
2023-06-21 08:02:30 -0300

    date added

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,13 +3,15 @@ import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
   const { t } = useTranslation();
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <footer className='bg-defaultBlack2'>
       <div className='default-container'>
         <div className='flex text-base py-6 flex-col lg:flex-row place-items-center'>
           <div className='flex flex-grow place-items-center'>
             <span className='px-2 text-center mb-2 lg:mb-0'>
-              Copyright © 2022 <span className='font-bold'>{t('footer.copyright.name')}</span>.{' '}
+              Copyright © {year} <span className='font-bold'>{t('footer.copyright.name')}</span>.{' '}
               {t('footer.copyright.all-rights-reserved')}
             </span>
           </div>
